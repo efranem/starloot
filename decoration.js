8c99c25556c135e86a40f5b8f77ddea47308703a
mrzeon <mrzeon@gmail.com>
2012-07-05 14:30:42 +0000

    Fixed crater

--- decoration.js
+++ decoration.js
@@ -2,7 +2,7 @@ function Crater(x, y){
 	this.x = x;
 	this.y = y;
 	this.img = new Image;
-	this.img.src = 'sprites/decoration/crater_2x2.png';
+	this.img.src = 'sprites/decoration/crater_4x4.png';
 	this.paint = function(ctx){
         ctx.drawImage(this.img, 0, 0, 512, 256, this.x, this.y, 512, 256);
 	};
