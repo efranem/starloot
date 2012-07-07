1189003ae393959e4aae88eaa96e593e3c6f8095
khanser@gmail.com <khanser@gmail.com@4a7841a3-5a9e-5372-77e0-f2686a090fee>
2012-07-07 07:53:38 +0000

    Added support for building collisions (web edition, server SVN is 1.6 my client is 1.8)

--- trunk/decoration.js
+++ trunk/decoration.js
@@ -1,13 +1,15 @@
 function Crater(x, y){
 	this.x = x;
 	this.y = y;
-	this.dim = [2,2];
+	this.dim = [4,2];
 	this.img = new Image;
 	this.img.src = 'sprites/decoration/crater_4x4.png';
 	this.paint = function(ctx){
         ctx.drawImage(this.img, 0, 0, 512, 256, this.x, this.y, 512, 256);
 		ctx.strokeStyle="red";
-		ctx.strokeRect(this.x,this.y,512,256);
+		var cdx = (this.dim[0]*128);
+		var cdy = (this.dim[1]*128);
+		ctx.strokeRect(this.x,this.y,cdx,cdy);
 	};
 	this.update = function(){
 	};
