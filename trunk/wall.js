e9fabfe5088e18556c2fa9f2bfcdb7627a580de0
khanser@gmail.com <khanser@gmail.com@4a7841a3-5a9e-5372-77e0-f2686a090fee>
2012-07-07 07:55:31 +0000

    Added support for building collisions (web edition, server SVN is 1.6 my client is 1.8)

--- trunk/wall.js
+++ trunk/wall.js
@@ -1,7 +1,7 @@
 function Wall(x, y, orientation){
 	this.x = x;
 	this.y = y;
-	this.dim = [1,1];
+	this.dim = [2,2];
     this.orientation = orientation;
 	this.img = new Image;
 	this.img.src = 'sprites/buildings/wall_01.png';
@@ -15,7 +15,9 @@ function Wall(x, y, orientation){
 	this.paint = function(ctx){
         ctx.drawImage(this.img, this.sprites[this.orientation][0], this.sprites[this.orientation][1], 256, 256, this.x, this.y, 256, 256);
 		ctx.strokeStyle="red";
-		ctx.strokeRect(this.x,this.y,256,256);
+		 var cdx = (this.dim[0]*128);
+		var cdy = (this.dim[1]*128);
+		ctx.strokeRect(this.x,this.y,cdx,cdy);
 	};
 	
 	this.update = function(){
