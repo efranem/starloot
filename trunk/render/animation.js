7ec9f25dc8b626a21b9870a7b6dd14d5eb66a6d4
alkaitz <alkaitz@4a7841a3-5a9e-5372-77e0-f2686a090fee>
2012-07-18 21:46:37 +0000

    - Added a new code file for OO utilities - Added function to complete an inheritance and calling the public constructor with parameters - Make a new drawable and animation class (with an example of inheritance between them)

--- trunk/render/animation.js
+++ trunk/render/animation.js
@@ -1,39 +1,29 @@
 /**
  file - Name to image file to draw
- x - Central x coord
- y - Central y coord 
- size - [pX:, pY:] Number of pixels in each dimension of full sprite
+ x - Origin x coord
+ y - Origin y coord 
+ size - [y:, y:] Number of pixels in each dimension of full sprite
  numFrames - [numX:, numY:] Number of frames in each dimension
  timeBetweenFrames - time between each couple of frames in ms
 */
 function Animation(file, x, y, size, numFrames, timeBetweenFrames){
-    this.img = document.getElementById(file);
-    this.currentSprite = 0;
-	this.spriteSize = {x: size.pX / numFrames, y: size.pY};
-    this.x = x - (this.spriteSize.x / 2); // x is the origin of drawing
-	this.y = y - (this.spriteSize.y / 2); // y is the origin of drawing
-    this.numFrames = numFrames;
-    this.sprites = new Array;
-    for (var i = 0; i < this.numFrames; i++){
+	/**
+		Own properties
+	*/
+	this.currentSprite = 0;
+	this.spriteSize = {x: size.x / numFrames, y: size.y};
+	this.numFrames = numFrames;
+	this.sprites = new Array;
+    for (var i = 0; i < numFrames; i++){
         this.sprites.push( {x: this.spriteSize.x * i, y: this.spriteSize.y * 0} );
     };
-    this.timeBetweenFrames = timeBetweenFrames || 40; // Updates each 40 ms (24 fps aprox)
-	this.timePending = this.timeBetweenFrames;
-    
-    /**
-		Sets the current central point of the sprite
-	*/
-	this.setCentralPoint = function(point){
-		this.x = point.x - (this.spriteSize.x / 2);
-		this.y = point.y - (this.spriteSize.y / 2);		
-	};
+	this.timeBetweenFrames = timeBetweenFrames || 40; // Updates each 40 ms (24 fps aprox)
+	this.timePending = timeBetweenFrames;
 	
 	/**
-		Sets the current frame to draw
+		Drawable inheritance calling public constructor with parameters
 	*/
-	this.setCurrentFrame = function(frame){
-		this.currentSprite = frame;
-	};
+	this.inherits( Drawable, x, y, file, this.spriteSize );
     
     /**
 		Updates the current frame to draw
@@ -55,28 +45,17 @@ function Animation(file, x, y, size, numFrames, timeBetweenFrames){
 };
 
 /**
-    Paints the current sprite on screen
+	Inheritance from Drawable
 */
-Animation.prototype.paint = function(ctx){
-    ctx.drawImage(this.img,
-   			this.sprites[ this.currentSprite ].x,
-			this.sprites[ this.currentSprite ].y,
-			this.spriteSize.x,
-			this.spriteSize.y,
-			this.x,
-			this.y,
-			this.spriteSize.x,
-			this.spriteSize.y);
-};
+Animation.prototype = new Drawable();
+Animation.prototype.constuctor = Animation;
 
 /**
-    Returns true if custom pixel is visible and false if it is transparent
+    Override paint drawable method
 */
-Animation.prototype.isPaintedPixel = function(pixel){
-    // Let's see if it's inside the painted zone
-    if (this.x > pixel.x || pixel.x > this.x + this.spriteSize.x ||
-        this.y > pixel.y || pixel.y > this.y + this.spriteSize.y)
-        return false;
-    
-    return true;
+Animation.prototype.paint = function(ctx){
+	Drawable.prototype.paint.call(this, ctx, 
+						this.sprites[ this.currentSprite ].x, 
+						this.sprites[ this.currentSprite ].y);
 };
+
---
+++ trunk/render/drawable.js
@@ -0,0 +1,40 @@
+/**
+ x - Origin x coord
+ y - Origin y coord
+ file - Name to id  of image file to draw
+ size - [x: , y:] Size in pixels in each direction to draw
+*/
+function Drawable(x, y, file, size){
+	this.img = document.getElementById(file);
+	this.x = x;
+	this.y = y;
+	this.size = size;
+};
+
+/**
+	Returns true if custom pixel is visible and false if it is transparent
+*/
+Drawable.prototype.isPaintedPixel = function(pixel){
+	// Let's see if it's inside the painted zone
+	if (this.x > pixel.x || pixel.x > this.x + this.spriteSize.x ||
+		this.y > pixel.y || pixel.y > this.y + this.spriteSize.y)
+		return false;
+	
+	return true;
+};
+
+/**
+	Paints the current sprite on screen
+*/
+Drawable.prototype.paint = function(ctx, offsetX, offsetY){
+	if (this.img.complete)
+		ctx.drawImage(this.img,
+			offsetX,
+			offsetY,
+			this.size.x,
+			this.size.y,
+			this.x,
+			this.y,
+			this.size.x,
+			this.size.y);
+};
