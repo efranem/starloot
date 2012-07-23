791664765cb1fe85731bc789e9bef094cbbaf68f
alkaitz <alkaitz@4a7841a3-5a9e-5372-77e0-f2686a090fee>
2012-07-23 21:32:51 +0000

    - Added Node prototype - Changed animation and drawable to keep private members

--- trunk/render/drawable.js
+++ trunk/render/drawable.js
@@ -5,34 +5,58 @@
  size - [x: , y:] Size in pixels in each direction to draw
 */
 function Drawable(file, x, y, size){
-	this.img = document.getElementById(file);
-	this.x = x;
-	this.y = y;
-	this.size = size;
+	/**
+		Own properties
+	*/
+	var _img = document.getElementById(file);
+	var _origin = {x: x, y: y};
+	var _size = size;
     
     /**
 		Set origin point to draw
 	*/
-    this.setOrigin = function(x, y){
-        this.x = x;
-        this.y = y;
+    this.setOrigin = function(X, Y){
+    	_origin = {x: X, y: Y};
+    };
+    
+    /**
+     	Gets origin point
+     */
+    this.getOrigin = function(){
+    	return _origin;
     };
     
     /**
 		Set size to draw
 	*/
     this.setSize = function(X, Y){
-        this.size = {x: X, y: Y};
+        _size = {x: X, y: Y};
     };
+    
+    /**
+     	Gets the size of the drae 
+     */
+    this.getSize = function(){
+    	return _size;
+    };
+        
+    /**
+     	Get image to paint 
+     */
+    this.getImage = function(){
+    	return _img;
+    }
 };
 
 /**
 	Returns true if custom pixel is visible and false if it is transparent
 */
 Drawable.prototype.isPaintedPixel = function(pixel){
+	var origin 	= this.getOrigin();
+	var size 	= this.getSize();
 	// Let's see if it's inside the painted zone
-	if (this.x > pixel.x || pixel.x > this.x + this.spriteSize.x ||
-		this.y > pixel.y || pixel.y > this.y + this.spriteSize.y)
+	if (origin.x > pixel.x || pixel.x > origin.x + size.x ||
+		origin.y > pixel.y || pixel.y > origin.y + size.y)
 		return false;
 	
 	return true;
@@ -42,14 +66,17 @@ Drawable.prototype.isPaintedPixel = function(pixel){
 	Paints the current sprite on screen
 */
 Drawable.prototype.paint = function(ctx, offsetX, offsetY){
-	if (this.img.complete)
-		ctx.drawImage(this.img,
+	var img 	= this.getImage();
+	var origin 	= this.getOrigin();
+	var size 	= this.getSize();
+	if (img.complete)
+		ctx.drawImage(img,
 			offsetX,
 			offsetY,
-			this.size.x,
-			this.size.y,
-			this.x,
-			this.y,
-			this.size.x,
-			this.size.y);
+			size.x,
+			size.y,
+			origin.x,
+			origin.y,
+			size.x,
+			size.y);
 };
