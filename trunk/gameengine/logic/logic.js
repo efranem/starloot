99fa83f6a708f8c32172627d5dfa64d24efd9695
alkaitz@gmail.com <alkaitz@gmail.com@4a7841a3-5a9e-5372-77e0-f2686a090fee>
2013-04-18 10:02:50 +0000

    - Basic split on logic and render.

---
+++ trunk/gameengine/logic/logic.js
@@ -0,0 +1,33 @@
+//  DELETE ME
+var nodeArray = new Array;
+
+for (var j = 0; j < 10; j++){
+    for (var i = 0; i < 14; i++){
+        var node = new Node('scout' + i + '-' + j, (i * 128) + 64, (j * 128) + 16, 1, new Size2D(8, 4), new Size2D(128, 128), ['reconn']);
+        node.play();
+        nodeArray.push( node );
+    };
+}
+/////////////
+
+
+function GameLogic(){
+    this.logicTime = 0;
+
+    this.logicTick = function(elapsedTime){
+        // Start timing
+        var time1 = new Date().getTime();
+
+        //  DELETE ME
+        for (var i = 0; i < nodeArray.length; i++){
+            var temp = nodeArray[i];
+            temp.update( 20 );
+        };
+        /////////////
+        
+        this.logicTime = new Date().getTime() - time1;
+    };
+    
+}; 
+
+logic = new GameLogic;
--- trunk/gameengine/renderer/renderer.js
+++ trunk/gameengine/renderer/renderer.js
@@ -3,20 +3,6 @@ function Renderer(){
 	
     this.timeRendering = 0;
 
-
-	//  DELETE ME
-    var nodeArray = new Array;
-
-    for (var j = 0; j < 10; j++){
-    	for (var i = 0; i < 14; i++){
-			var node = new Node('scout' + i + '-' + j, (i * 128) + 64, (j * 128) + 16, 1, new Size2D(8, 4), new Size2D(128, 128), ['reconn']);
-			node.play();
-			nodeArray.push( node );
-		};
-    }
-	/////////////
-
-
     this.renderFrame = function(){
         // Start timing
         var time1 = new Date().getTime();
@@ -28,7 +14,6 @@ function Renderer(){
         //  DELETE ME
         for (var i = 0; i < nodeArray.length; i++){
         	var temp = nodeArray[i];
-        	temp.update( 20 );
         	temp.paint( ctx );
         };
         /////////////
